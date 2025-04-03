dd6bb0e39729bbef51eaf3ad5f62efe2b1380469
orevillo16 <156692530+orevillo16@users.noreply.github.com>
2025-04-03 13:03:22 +0800

    FEAT: add classSubject

--- JS/teacher.js
+++ JS/teacher.js
@@ -2,8 +2,12 @@ const createNewClass = () => {
     const boxClass = document.querySelector('.box'); 
     const container = document.createElement('div');
     const classInput = document.createElement('input');
+    const classSubject = document.createElement('input');
 
     console.log(classInput);
     boxClass.appendChild(container);
-    container.appendChild(classInput);   
+    container.appendChild(classInput);
+    container.appendChild(classSubject);   
 };
+
+
