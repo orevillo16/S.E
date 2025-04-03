4c4c0d699a03ac7e4f07465d2439f09051d64d19
orevillo16 <156692530+orevillo16@users.noreply.github.com>
2025-04-03 12:56:41 +0800

    FEAT: createNewClass funtion

---
+++ JS/teacher.js
@@ -0,0 +1,9 @@
+const createNewClass = () => {
+    const boxClass = document.querySelector('.box'); 
+    const container = document.createElement('div');
+    const classInput = document.createElement('input');
+
+    console.log(classInput);
+    boxClass.appendChild(container);
+    container.appendChild(classInput);   
+};
