fd2cd6cfb937b25545c3c551e44f0dc7ee3e1438
orevillo16 <156692530+orevillo16@users.noreply.github.com>
2025-05-04 11:07:09 +0800

    FEAT: enhance createNewClass function and update CSS for button styling

--- JS/teacher.js
+++ JS/teacher.js
@@ -3,11 +3,36 @@ const createNewClass = () => {
     const container = document.createElement('div');
     const classInput = document.createElement('input');
     const classSubject = document.createElement('input');
+    const buttonContainer = document.createElement('div');
+    const createClass = document.createElement('button')
 
-    console.log(classInput);
+    classInput.classList.add('CNC_Input');  
+    classInput.placeholder = 'Enter Class/Section';
+    classSubject.classList.add('CNC_Input');
+    classSubject.placeholder = 'Enter Subject';
+    buttonContainer.classList.add('CreateClass-button-box');
+    createClass.classList = ('createClass');
+    createClass.textContent = 'Create Class';
+    
     boxClass.appendChild(container);
     container.appendChild(classInput);
-    container.appendChild(classSubject);   
+    container.appendChild(classSubject);  
+    boxClass.appendChild(buttonContainer);
+    buttonContainer.appendChild(createClass); 
+
+
+
+    const addtable = document.createElement('table')
+    
+    const headerRow = document.createElement('tr');
+    const headers = ['NAME', 'STUDENT ID NUMBER', 'GRADE', ''];
+    
+    container.appendChild(addtable)
+    addtable.appendChild(headerRow)
+    headerRow.appendChild(headers)
+  
+
+    
 };
 
 
